596ddcbb93a22ce8d53a65be9582fe14010faf8c
omerHamid <66900959+omerHamid@users.noreply.github.com>
2021-08-23 14:39:35 +0530

    nothing

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{background: 'sky'}} className="App">
+      <h1>this is the home page</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus eligendi ex in excepturi expedita beatae?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio voluptas commodi eaque, rem repellat tempore facilis eum fuga sed numquam!</p>
+    </div>
+  );
+}
+
+export default App;
